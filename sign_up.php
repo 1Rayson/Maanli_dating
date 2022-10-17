0a1a717e893997bfb15e5d794849fca853752d8b
1Rayson <andersskrydstrup@gmail.com>
2022-10-17 11:23:23 +0200

    form created

--- sign_up.php
+++ sign_up.php
@@ -12,6 +12,12 @@
     <title>Sign up</title>
 </head>
 <body>
-    <form action=""></form>
+      <form action="sign-up.php" method="post">
+        <input type="text" name="firstName" placeholder="Firstname">
+        <input type="text" name="lastName" placeholder="Lastname">
+        <input type="text" name="userName" placeholder="Username">
+        <input type="text" name="password" placeholder="Password">
+        <input type="submit">
+    </form>
 </body>
 </html>
