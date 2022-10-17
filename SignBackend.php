fef6930400045d9a41db3967fb2b190d164adf00
1Rayson <andersskrydstrup@gmail.com>
2022-10-17 14:16:54 +0200

    Sign up options

--- SignBackend.php
+++ SignBackend.php
@@ -1,16 +1,16 @@
 <?php
     session_start();
     include("../mySQL.php");
-    $firstName = (isset($_REQUEST['FirstName'])) ? $_REQUEST['FirstName']: "";
-    $lastName = (isset($_REQUEST['LastName'])) ? $_REQUEST['LastName']: "";
-    $userName = (isset($_REQUEST['UserName'])) ? $_REQUEST['UserName']: "";
+    $firstName = (isset($_REQUEST['firstName'])) ? $_REQUEST['FirstName']: "";
+    $lastName = (isset($_REQUEST['lastName'])) ? $_REQUEST['LastName']: "";
+    $userName = (isset($_REQUEST['userName'])) ? $_REQUEST['UserName']: "";
     $password = (isset($_REQUEST['password'])) ? $_REQUEST['password']: "";
 
     if($firstName !="" && $lastName !="" && $userName !="" && $password !=""){
         $insertSQL="INSERT INTO Users (firstName, lastName, age, gender, height, username, pw)
         VALUES
         ('$firstName', '$lastName', '$userName', '$password')";
-        $mySQL->query($insertSQL); 
+        $mySQL->query($insertSQL);
         header('Location: index.php?response=success');
     } else{
         header('Location: index.php?response=Failure');
--- sign_up.php
+++ sign_up.php
@@ -1,6 +1,6 @@
  <?php
-        session_start();
-        include("../mySQL.php");
+    session_start();
+    include("classes/mySQL.php");
     ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -16,9 +16,19 @@
       <form action="sign-up.php" method="post">
         <input type="text" name="firstName" placeholder="Firstname">
         <input type="text" name="lastName" placeholder="Lastname">
+        <input type="text" name="age" placeholder="Age">
+        <select id="gender "name="gender">
+                <option value="none" selected>Gender</option>
+                <option value="male">Male</option>
+                <option value="female">Female</option>
+                <option value="nonbinary">Nonbinary</option>
+                <option value="other">Other</option>
+            </select>
+        <label for="height">Height in CM:</label>
+        <input type="text" name="height" place="Height">
         <input type="text" name="userName" placeholder="Username">
         <input type="text" name="password" placeholder="Password">
-        <input type="submit">
+        <input type="submit" value="Sign Up">
     </form>
 </body>
 </html>
