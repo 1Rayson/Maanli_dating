f46c9cb7b710b8871c2fd908e065ccd8adb23140
MarBProduction <MarBProduction@users.noreply.github.com>
2022-10-17 11:33:35 +0200

    login form. login style
    - login form created
    - login style created

--- Login.php
+++ Login.php
@@ -4,9 +4,21 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="login-style.css">
+    <title>Login</title>
 </head>
-<body>
-    
+<body id="login-body">
+    <section id="login-section">
+        <form action="login-back.php" method="post" id="login-form">
+            <label>
+                <p>Username</p>
+                <input type="text" name="userName" id="login-username">
+            </label>
+            <label>
+                <p>Password</p>    
+                <input type="password" name="password" id="login-password">
+            </label>
+        </form>
+    </section>    
 </body>
 </html>
