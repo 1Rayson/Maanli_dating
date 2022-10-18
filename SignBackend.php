0880826325c10f8d7bb9b0d98cc0a3fb68b804a9
1Rayson <andersskrydstrup@gmail.com>
2022-10-18 11:15:47 +0200

    sign up code
    Should in theory work now

--- SignBackend.php
+++ SignBackend.php
@@ -1,16 +1,18 @@
 <?php
     session_start();
     include("../mySQL.php");
-    $firstName = (isset($_REQUEST['firstName'])) ? $_REQUEST['FirstName']: "";
-    $lastName = (isset($_REQUEST['lastName'])) ? $_REQUEST['LastName']: "";
-    $userName = (isset($_REQUEST['userName'])) ? $_REQUEST['UserName']: "";
+    $firstName = (isset($_REQUEST['firstName'])) ? $_REQUEST['firstName']: "";
+    $lastName = (isset($_REQUEST['lastName'])) ? $_REQUEST['lastName']: "";
+    $age = (isset($_REQUEST['age'])) ? $_REQUEST['age']: "";
+    $gender = (isset($_REQUEST['gender'])) ? $_REQUEST['gender']: "";
+    $height = (isset($_REQUEST['height'])) ? $_REQUEST['height']: "";
+    $userName = (isset($_REQUEST['userName'])) ? $_REQUEST['userName']: "";
     $password = (isset($_REQUEST['password'])) ? $_REQUEST['password']: "";
 
-    if($firstName !="" && $lastName !="" && $userName !="" && $password !=""){
-        $insertSQL="INSERT INTO Users (firstName, lastName, age, gender, height, username, pw)
-        VALUES
-        ('$firstName', '$lastName', '$userName', '$password')";
-        $mySQL->query($insertSQL);
+    if($firstName !="" && $lastName !="" && $age !="" && $gender !="" && $height !="" && $userName !="" && $password !=""){
+        $passEncrypt = password_hash($password, PASSWORD_DEFAULT);
+        $userSQL = "CALL InsertMaanliUserData('$firstName', '$lastName','$age', '$gender', '$height', '$userName', '$passEncrypt');";    
+        $mySQL->query($userSQL);
         header('Location: index.php?response=success');
     } else{
         header('Location: index.php?response=Failure');
