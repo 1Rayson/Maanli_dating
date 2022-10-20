a80035c5ed300584c45b374cc8e6ed7fac140614
MarBProduction <MarBProduction@users.noreply.github.com>
2022-10-20 14:22:16 +0200

    Prepared logic for database read

--- index-backend.php
+++ index-backend.php
@@ -1,3 +1,11 @@
 <?php
+    session_start();
+
+    
+    if($_GET['nextMatch']){
+        $_SESSION['match']++;
+        header("location: index.php");
+    }
+
 
 ?>
--- index.php
+++ index.php
@@ -3,6 +3,30 @@
     
     if(!isset($_SESSION['userToken'])) header("location: Login.php");
 
+    /* temperary array of matches */
+    $testUsers = [
+        (object) [
+            'name' => 'Dick Sucky',
+            'age' => 25,
+            'sexualPreference' => 'female',
+            'gender' => 'male',
+            'interest1' => 'cars',
+            'interest2' => 'hair',
+            'interest3' => 'party'
+        ],
+        (object) [
+            'name' => 'Justine Case',
+            'age' => 22,
+            'sexualPreference' => 'male',
+            'gender' => 'female',
+            'interest1' => 'make-up',
+            'interest2' => 'social media',
+            'interest3' => 'party'
+        ]
+    ];
+
+    $match = isset($_SESSION['match']) && $_SESSION['match'] < sizeof($testUsers) ? $_SESSION['match'] : $_SESSION['match'] = 0;
+    
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -16,13 +40,15 @@
 <body>
     <section>
         <img id="match_picture" src="" alt=""> <!-- picture of match -->
-        <h2 id="match_name">Name</h2>
-        <p id="match_age">Age</p>
+        <h2 id="match_name"><?php echo $testUsers[$match]->name ?></h2>
+        <p id="match_gender"><?php echo $testUsers[$match]->gender ?></p>
+        <p id="match_age"><?php echo $testUsers[$match]->age ?></p>
         <div id="match_interest">
-            <div class="match_interst-item">Interest 1</div>
-            <div class="match_interst-item">Interest 2</div>
-            <div class="match_interst-item">Interest 3</div>
+            <div class="match_interst-item"><?php echo $testUsers[$match]->interest1 ?></div>
+            <div class="match_interst-item"><?php echo $testUsers[$match]->interest2 ?></div>
+            <div class="match_interst-item"><?php echo $testUsers[$match]->interest3 ?></div>
         </div>
+        <a id="match_next-button" href="index-backend.php?nextMatch=true">Next match</a>
     </section>
 </body>
 </html>
