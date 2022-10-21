0361108222b15a996415327591b60289ed84547f
1Rayson <andersskrydstrup@gmail.com>
2022-10-21 16:22:19 +0200

    (additional pylons) logout button constructed

--- backend.php
+++ backend.php
@@ -1,7 +1,11 @@
 <?php
 session_start();
 include("classes/mySQL.php");
+$database = new MySQL(true);
 
-
+if(isset($_GET['logout']) && $_GET['logout'] == true ){
+    $_SESSION['userToken'] = 0;
+    header('location: Login.php');
+}
 
 ?>
