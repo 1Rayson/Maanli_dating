9df94f29aa48506b944eba8a819039e91c3be5a5
1Rayson <andersskrydstrup@gmail.com>
2022-10-21 10:54:47 +0200

    users are no longer allowed to have a life (will be patched later)

--- SignBackend.php
+++ SignBackend.php
@@ -9,16 +9,17 @@
     $age = (isset($_REQUEST['age'])) ? $_REQUEST['age']: "";
     $gender = (isset($_REQUEST['gender'])) ? $_REQUEST['gender']: "";
     $height = (isset($_REQUEST['height'])) ? $_REQUEST['height']: "";
-    $userName = (isset($_REQUEST['userName'])) ? $_REQUEST['userName']: "";
-    $password = (isset($_REQUEST['password'])) ? $_REQUEST['password']: "";
     $younger = (isset($_REQUEST['younger'])) ? $_REQUEST['younger']: "";
     $older = (isset($_REQUEST['older'])) ? $_REQUEST['older']: "";
     $pGender = (isset($_REQUEST['prefer_gender'])) ? $_REQUEST['prefer_gender']: "";
-    $inter1 = (isset($_REQUEST['interest1'])) ? $_REQUEST['interest1']: "";
-    $inter2 = (isset($_REQUEST['interest2'])) ? $_REQUEST['interest2']: "";
-    $inter3 = (isset($_REQUEST['interest3'])) ? $_REQUEST['interest3']: "";
+    $userName = (isset($_REQUEST['userName'])) ? $_REQUEST['userName']: "";
+    $password = (isset($_REQUEST['password'])) ? $_REQUEST['password']: "";
     
-    if($firstName !="" && $lastName !="" && $age !="" && $gender !="" && $height !="" && $userName !="" && $password !="" && $younger !="" && $older !="" && $pGender !="" && $inter1 !="" && $inter2 !="" && $inter3 !=""){
+    /* $inter1 = (isset($_REQUEST['interest1'])) ? $_REQUEST['interest1']: "";
+    $inter2 = (isset($_REQUEST['interest2'])) ? $_REQUEST['interest2']: "";
+    $inter3 = (isset($_REQUEST['interest3'])) ? $_REQUEST['interest3']: ""; */
+    
+    if($firstName !="" && $lastName !="" && $age !="" && $gender !="" && $height !="" && $younger !="" && $older !="" && $pGender !="" && $userName !="" && $password !="" ){
         $passEncrypt = password_hash($password, PASSWORD_DEFAULT);
         $userSQL = "CALL InsertMaanliUserData('$firstName', '$lastName','$age', '$gender', '$height', '$userName', '$passEncrypt', '$younger', '$older', '$pGender', '$inter1', '$inter2', '$inter3' );";    
         $database->Query($userSQL);
