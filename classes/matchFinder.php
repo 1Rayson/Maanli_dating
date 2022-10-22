b2232398012a2bc9f1945cc3660f7b2d12d3ed8a
MarBProduction <MarBProduction@users.noreply.github.com>
2022-10-22 15:57:05 +0200

    added a matchfinder

---
+++ classes/matchFinder.php
@@ -0,0 +1,64 @@
+<?php
+
+    class matchFinder {
+        private $userToken;
+        private $database;
+        
+        function __construct($givenUserToken, $givenDatabase){
+            $this->userToken = $givenUserToken;
+            $this->database = $givenDatabase;
+        }
+        
+        public function getUserPreferences(){
+            $preferenceQuery = "
+                SELECT maanliUserPreference.prefer_gender, maanliUserPreference.minAge, maanliUserPreference.maxAge, maanliUserProfile.age
+                FROM maanliUserPreference 
+                LEFT JOIN maanliUserProfile 
+                ON maanliUserPreference.id = maanliUserProfile.id
+                WHERE maanliUserPreference.id = $this->userToken
+            ";
+            return $this->database->Query($preferenceQuery)->fetch_object();
+        }
+
+        public function findMatches($givenPreferences, $withInterest = true){
+            $matchQuery = "
+                SELECT id, firstName, lastName, age, gender 
+                FROM maanliUserProfile
+                WHERE id != $this->userToken
+            ";
+
+            if($givenPreferences->prefer_gender != "any")
+                $matchQuery .= "AND gender = '$givenPreferences->prefer_gender'";
+
+            $matchQuery .= "
+                AND age > $givenPreferences->age - $givenPreferences->minAge
+                AND age < $givenPreferences->age + $givenPreferences->maxAge
+            ";
+
+            $matchList = $this->database->Query($matchQuery);
+            return $matchList;
+
+            if($withInterest) {
+
+                for($i=0; $i<sizeof($matchList); $i++){
+                    $matchInterestQuery = "
+                        SELECT interestName, userID
+                        FROM maanliUserInterests
+                        WHERE ".$matchList[$i]->id." = userID
+                    ";
+                    $matchInterestFetch = $this->database->Query($matchInterestQuery);
+            
+                    $matchInterestList = [];
+                    while($row = $matchInterestFetch->fetch_object()) {
+                        $matchInterestList[] = $row->interestName;
+                    }
+            
+                    $matchList[$i]->matchInterestList = $matchInterestList;
+                }
+            }
+
+            return $matchList;
+        }
+    }
+
+?>
--- index.php
+++ index.php
@@ -2,47 +2,23 @@
     session_start();
     if(!isset($_SESSION['userToken'])) header("location: Login.php");
 
-    include("classes/mySQL.php");
-    $database = new MySQL(true);
+    include_once("classes/mySQL.php");
+    include_once("classes/matchFinder.php");
     $userToken = $_SESSION['userToken'];
-    $preferenceQuery = "
-        SELECT maanliUserPreference.prefer_gender, maanliUserPreference.minAge, maanliUserPreference.maxAge, maanliUserProfile.age
-        FROM maanliUserPreference 
-        LEFT JOIN maanliUserProfile 
-        ON maanliUserPreference.id = maanliUserProfile.id
-        WHERE maanliUserPreference.id = $userToken
-    ";
-    $userPreferences = $database->Query($preferenceQuery)->fetch_object();
+    $database = new MySQL(true);
+    $matchFinder = new matchFinder($userToken, $database);
 
-    $matchQuery = "
-        SELECT id, firstName, lastName, age, gender, height 
-        FROM maanliUserProfile
-        WHERE id != $userToken 
-        AND gender = '$userPreferences->prefer_gender'
-        AND age > $userPreferences->age - $userPreferences->minAge
-        AND age < $userPreferences->age + $userPreferences->maxAge
-    ";
+    $userPreferences = $matchFinder->getUserPreferences();
 
-    $matchFetch = $database->Query($matchQuery);
+    $matchList = $matchFinder->findMatches($userPreferences);
+    var_dump($matchList);
+    exit;
 
-    $matchList = [];
-    while($row = $matchFetch->fetch_object()) {
-        $match = [];
-        $match['id'] = $row->id;
-        $match['firstName'] = $row->firstName;
-        $match['lastName'] = $row->lastName;
-        $match['age'] = $row->age;
-        $match['gender'] = $row->gender;
-        $match['height'] = $row->height;
-
-        $matchList[] = $match;
-    }
-
-    for($i=0; $i<sizeof($matchList); $i++){
+    /* for($i=0; $i<sizeof($matchList); $i++){
         $matchInterestQuery = "
             SELECT interestName, userID
             FROM maanliUserInterests
-            WHERE ".$matchList[$i]['id']." = userID
+            WHERE ".$matchList[$i]->id." = userID
         ";
         $matchInterestFetch = $database->Query($matchInterestQuery);
 
@@ -51,23 +27,8 @@
             $matchInterestList[] = $row->interestName;
         }
 
-        $matchList[$i]['matchInterestList'] = $matchInterestList;
-    }
-
-    /* 
-    maanliUserProfile
-        - firstName
-        - lastName
-        - age
-        - gender
-    maanliUserPreference
-        - prefer_gender
-        - minAge
-        - maxAge
-    maanliUserInterest
-        - interestName
-        - userID
-    */
+        $matchList[$i]->matchInterestList = $matchInterestList;
+    } */
 
     $matchArrayId = isset($_SESSION['matchArrayId']) && $_SESSION['matchArrayId'] < sizeof($matchList) ? $_SESSION['matchArrayId'] : $_SESSION['matchArrayId'] = 0;
     
@@ -104,13 +65,10 @@
             
             <p class="description-tag">Interests</p>
             <article id="match_interest">
-                <div class="match_interst-item"><?php echo $matchList[$matchArrayId]->interest1 ?></div>
-                <div class="match_interst-item"><?php echo $matchList[$matchArrayId]->interest2 ?></div>
-                <div class="match_interst-item"><?php echo $matchList[$matchArrayId]->interest3 ?></div>
                 <?php
-                    $matchInterest = $matchList[$matchArrayId]['matchInterestList'];
-                    
-                    for($i=0; $i<sizeof($matchInterest); $i++){
+                $matchInterest = $matchList[$matchArrayId]->matchInterestList;
+                
+                for($i=0; $i<sizeof($matchInterest); $i++){
                         echo "<div class='match_interst-item'>".$matchInterest[$i]."</div>";
                     }
                 ?>
