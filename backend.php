3bd3e1c2d112c3a34db13bb76179ee460f3b2576
MarBProduction <MarBProduction@users.noreply.github.com>
2022-10-23 15:47:24 +0200

    hotfix hotfix update

--- backend.php
+++ backend.php
@@ -123,7 +123,7 @@ if($action == 'update') {
         if($firstName !="" && $lastName !="" && $age !="" && $gender !="" && $height !="" && $minAge !="" && $maxAge !="" && $preferedGender !="" && $username !="" && $password !="" ){
     
             $passEncrypt = password_hash($password, PASSWORD_DEFAULT);
-            $userSQL = "CALL UpdateMaanliUserData('$user_id', '$firstName', '$lastName','$age', '$gender', '$height', '$minAge', '$maxAge', '$preferedGender', '$username', '$passEncrypt' );";
+            $userSQL = "CALL UpdateMaanliUserData('$user_id', '$firstName', '$lastName','$age', '$gender', '$height', '$preferedGender', '$minAge', '$maxAge', '$username', '$passEncrypt' );";
             $database->Query($userSQL);
 
             $deleteInterestQuery = "
